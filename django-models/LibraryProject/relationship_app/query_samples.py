0ba28fd33195f48a383cda41a0e960c37121854c
Hamisi <yuzalsif2@gmail.com>
2025-11-06 15:24:48 +0300

    fixed author query

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,7 +3,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query all books by a specific author
 def books_by_author(author_name):
      author = Author.objects.get(name=author_name)
-     return Book.objects.filter(author__name=author_name)
+     return Book.objects.filter(author=author)
 
 # List all books in a library
 def books_in_library(library_name):
