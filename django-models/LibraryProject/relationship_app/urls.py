59154687725f207acf2c1b5b13ba4f20815a15a1
Hamisi <yuzalsif2@gmail.com>
2025-11-06 16:29:22 +0300

    extend the Django User model to include user roles and develop views that restrict access based on these roles. Your task is to set up this system by creating a new model for user profiles, defining views with access restrictions, and configuring URL patterns.

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
+      path('admin-role/', views.admin_view, name='admin_view'),
+    path('librarian-role/', views.librarian_view, name='librarian_view'),
+    path('member-role/', views.member_view, name='member_view'),
+
 ]
