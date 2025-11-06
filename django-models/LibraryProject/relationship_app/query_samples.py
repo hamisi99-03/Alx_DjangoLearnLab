d1e4866ac4693152f2d649bd1b94dbd759f493a7
Hamisi <yuzalsif2@gmail.com>
2025-11-06 15:13:46 +0300

    Duplicated the previous project directory Introduction_ to_ Django, renamed it to django-models and added a new app named relationship_app where i defined models that showcase complex relationships between entities using ForeignKey, ManyToMany, and OneToOne fields.

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,13 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+# Query all books by a specific author
+def books_by_author(author_name):
+    return Book.objects.filter(author__name=author_name)
+
+# List all books in a library
+def books_in_library(library_name):
+    return Library.objects.get(name=library_name).books.all()
+
+# Retrieve the librarian for a library
+def librarian_for_library(library_name):
+    return Library.objects.get(name=library_name).librarian
