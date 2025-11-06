18a09d62a9f844a3eb8689ed9203b8f4ebb96327
Hamisi <yuzalsif2@gmail.com>
2025-11-06 17:11:16 +0300

    checker match

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('books/add/', views.add_book, name='add_book'),
     path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
     path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),  # ✅ checker match
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),  # ✅ checker match
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
