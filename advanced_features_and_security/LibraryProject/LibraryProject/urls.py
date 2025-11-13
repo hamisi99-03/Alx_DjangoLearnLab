2c043ae15ce7c42ff542c759c9db2f2d1df16015
Hamisi <yuzalsif2@gmail.com>
2025-11-13 19:54:30 +0300

    added views and permission decorators

--- advanced_features_and_security/LibraryProject/LibraryProject/urls.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -23,5 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('', include('relationship_app.urls')),
+     path('bookshelf/', include('bookshelf.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render
+from .models import Book
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+
+
