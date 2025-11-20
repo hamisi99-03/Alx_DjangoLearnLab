41d156fe4d66efb963639d4c371978ef54bfdae4
Hamisi <yuzalsif2@gmail.com>
2025-11-20 19:16:38 +0300

    replaced the simple list view created previously with a full set of CRUD operations using DRF’s ViewSets. This will allow clients to not only retrieve but also create, update, and delete books via your API.

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,18 @@
-# api/urls.py
+
 from django.urls import path
+from django.urls import include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
 from .views import BookList
 
+#configure a router for the viewset
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
+
 urlpatterns = [
+    #route for the ListAPIView
     path('books/', BookList.as_view(), name='book-list'),
+    #route for the ViewSet to include all CRUD operations
+    path('', include(router.urls)),  # include the router URLs
 ]
