1a061fa3b4588fb3bbfd280b2d425e20867d6795
Hamisi <yuzalsif2@gmail.com>
2025-12-04 14:43:13 +0300

    configured urls and created simple views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+# simple views for login and registration pages
+def login_view(request):
+    return render(request, 'blog/login.html')
+
+def register_view(request):
+    return render(request, 'blog/register.html')
