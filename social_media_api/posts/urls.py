b2b775cb45df8efdb874a5812546bc5097452411
Hamisi <yuzalsif2@gmail.com>
2025-12-10 18:47:51 +0300

    explicity contain feed/

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,4 +1,3 @@
-
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
@@ -10,5 +9,9 @@ router.register(r'posts', PostViewSet, basename='post')
 router.register(r'comments', CommentViewSet, basename='comment')
 
 urlpatterns = [
+    # Explicit feed path
+    path('feed/', PostViewSet.as_view({'get': 'feed'}), name='feed'),
+
+    # Router endpoints (posts/, comments/, etc.)
     path('', include(router.urls)),
 ]
